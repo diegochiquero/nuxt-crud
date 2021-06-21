4c86c5e3a6e4a615e7006170b2c0af3aaa15bf54
diegochiquero <chiquerodiego@gmail.com>
2021-06-21 19:41:35 +0200

    docs: change docker image name

--- client/client-scripts/create-client-image.sh
+++ client/client-scripts/create-client-image.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 docker rm -f nuxt_crud_client
-docker rmi -f nuxt_crud_client
-sudo docker image build -t nuxt_crud_client ..
+docker rmi -f nuxt-crud-client
+sudo docker image build -t nuxt-crud-client ..
 
 # By specifying #!/usr/bin/env bash
 # you specify exactly which interpreter will be used to run the script on a particular system.
 # Only run when it goes to production
+
+# Clarification: nuxt-crud-client is the docker image name and nuxt_crud_client is the container name
 
--- server/server-scripts/create-server-image.sh
+++ server/server-scripts/create-server-image.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 docker rm -f nuxt_crud_server
-docker rmi -f nuxt_crud_server
-sudo docker image build -t nuxt_crud_server ..
+docker rmi -f nuxt-crud-server
+sudo docker image build -t nuxt-crud-server ..
 
 # By specifying #!/usr/bin/env bash
 # you specify exactly which interpreter will be used to run the script on a particular system.
 # Only run when it goes to production
 
+# Clarification: nuxt-crud-server is the docker image name and nuxt_crud_server is the container name
