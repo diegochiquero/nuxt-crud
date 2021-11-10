71706ab09fe7385fe0259b87a294b4ddfff479c0
diegochiquero <chiquerodiego@gmail.com>
2021-11-10 18:21:31 +0100

    docs: remove sudo statement

--- client/client-scripts/create-client-image.sh
+++ client/client-scripts/create-client-image.sh
@@ -2,7 +2,7 @@
 
 docker rm -f nuxt_crud_client
 docker rmi -f nuxt-crud-client
-sudo docker image build -t nuxt-crud-client ..
+docker image build -t nuxt-crud-client ..
 
 # By specifying #!/usr/bin/env bash
 # you specify exactly which interpreter will be used to run the script on a particular system.
