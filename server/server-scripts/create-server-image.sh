efe7155cb2a4cabe0ee342cc44da06e44ae24833
diegochiquero <chiquerodiego@gmail.com>
2021-11-11 16:34:23 +0100

    docs: remove sudo statement

--- server/server-scripts/create-server-image.sh
+++ server/server-scripts/create-server-image.sh
@@ -2,7 +2,7 @@
 
 docker rm -f nuxt_crud_server
 docker rmi -f nuxt-crud-server
-sudo docker image build -t nuxt-crud-server ..
+docker image build -t nuxt-crud-server ..
 
 # By specifying #!/usr/bin/env bash
 # you specify exactly which interpreter will be used to run the script on a particular system.
